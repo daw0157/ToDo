a1b71cacec3723db50a7918a82c08cb1dbb80dd1
Dan Wolterbeek <dawolterbeek@gmail.com>
2017-01-24 10:01:00 -0500

    Initial commit

---
+++ app/src/main/java/apps/daw/com/todo/model/ToDo.java
@@ -0,0 +1,14 @@
+package apps.daw.com.todo.model;
+
+/**
+ * Created by Dan on 1/24/2017.
+ */
+
+public class ToDo {
+
+    private int id;
+    private String task;
+    private boolean completed;
+
+
+}
